2431078754dccbb8e3ea3493a992a90b69b032e9
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2025-10-21 10:40:25 -0600

    Remove old codecov for coverage reports

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-SimpleCov.start
-
-if ENV.fetch('CI', nil) == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'jekyll/tasks'
 
